db9bc8987ff7bc465d7bfdb566cc312f3394635f
ryantixier <123503138+ryantixier@users.noreply.github.com>
2024-03-12 14:20:29 -0600

    added starter content and formatting for About.jsx; all other files code cleanup

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,17 @@
 
 // general
 import * as React from "react";
+import { useEffect } from "react";
+
+// animations
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// MUI components
+import IconButton from "@mui/material/IconButton";
+
+// MUI icons
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 // local
 
@@ -9,17 +20,56 @@ import * as React from "react";
 import "../assets/App.css";
 import "../assets/imageSource.css";
 
+// INLINE...
+// INLINE...
+
+// ... styles
+const iconButtonStyle = {
+  zIndex: 11,
+  color: "white",
+  background: "#e0e0e080",
+  "&:hover": {
+    color: "black",
+    background: "#e0e0e0",
+  },
+};
+
 export const About = () => {
+  useEffect(() => {
+    AOS.init({ once: true });
+  }, []);
+
   //Hooks
 
   //Functions
+  const scrollToPageContent = () => {
+    const pageContentElement = document.getElementById("pageContent");
+    if (pageContentElement) {
+      pageContentElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <>
-      <h1 className="page-intro">Greetings, Earthling!</h1>
-      <h2>Greetings, Earthling!</h2>
-      <div style={{ height: "1000px" }}>
-        {/* this is just to make the page scrollable */}
+      <div>
+        <div className="page-intro">
+          <h1 data-aos="fade-right" data-aos-duration="700">
+            Greetings, Earthling!
+          </h1>
+          <h4 data-aos="fade-left" data-aos-duration="1200">
+            You've made it Ryan Tixier's online portfolio.
+          </h4>
+          <div data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="800">
+            <IconButton
+              onClick={scrollToPageContent}
+              color="inherit"
+              sx={iconButtonStyle}
+            >
+              <ArrowDownwardIcon fontSize="large" color="inherit" />
+            </IconButton>
+          </div>
+        </div>
+        <div id="pageContent"></div>
       </div>
     </>
   );
